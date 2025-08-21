03f4e8eec439c4d28849640f0f35867acada6ad0
Aayush-7804 <aayushranapariya@gmail.com>
2025-08-21 16:25:22 +0530

    basic routing between pages

--- src/Components/MainNav.jsx
+++ src/Components/MainNav.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function MainNav() {
+function Button({ children, path, isSelected, onSelect }) {
   return (
-    <div>
-        <ul className='flex gap-10'>
-            <li>
-                <Link to='project-list'>Project list</Link>
-            </li>
-            <li>
-                <Link to='completed'>Completed Projects</Link>
-            </li>
-            <li>
-                <Link to='uncompleted'>Uncompleted Projects</Link>
-            </li>
+    <>
+      <li>
+        <Link to={path}>
+          <button className="px-7 py-2" onClick={onSelect}>
+            {children}
+          </button>
+        </Link>
+        {isSelected && (
+          <div className="border-b-4 border-[var(--primary)] rounded-t-full" />
+        )}
+      </li>
+    </>
+  );
+}
+export default function MainNav() {
+  const [selected, setSelected] = useState(0);
+  
+  return (
+    <header className="border-b border-[var(--alternative)]">
+      <h1 className="text-4xl">Project Management</h1>
+      <nav>
+        <ul className="flex gap-10">
+          <Button
+            path="project-list"
+            isSelected={selected === 0}
+            onSelect={() => setSelected(0)}
+          >
+            Project list
+          </Button>
+          <Button
+            path="completed"
+            isSelected={selected === 1}
+            onSelect={() => setSelected(1)}
+          >
+            Completed Project
+          </Button>
+          <Button
+            path="uncompleted"
+            isSelected={selected === 2}
+            onSelect={() => setSelected(2)}
+          >
+            Uncompleted Project
+          </Button>
         </ul>
-    </div>
-  )
+      </nav>
+    </header>
+  );
 }
